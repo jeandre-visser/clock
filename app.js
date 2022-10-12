42e1ff4d6af36f0ebd4d6cbcde405212a8e5aed8
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-11 19:56:15 -0600

    If a single digit number, then add a zero before hand

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ if (hr > 12) {
   hr = hr - 12
 };
 
+hr = (hr < 10) ? '0' + hr : hr;
+min = (min < 10) ? '0' + min : min;
+sec = (sec < 10) ? '0' + sec : sec;
+
 hrs.innerHTML = hr;
 mins.innerHTML = min;
 secs.innerHTML = sec;
