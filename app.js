56ebf1948cc4c5ecd87c7e87e9f046213c0dbfdb
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-11 19:39:56 -0600

    Methods for hour, minute, and seconds

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ let hrs = document.getElementById('hrs');
 let mins = document.getElementById('mins')
 let secs = document.getElementById('secs')
 let am = document.getElementById('am');
+
+let hr = new Date().getHours();
+let min = new Date().getMinutes();
+let sec = new Date().getSeconds();
