a19110fd850ec03b8a9168af625f1422efcf3c66
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-11 19:49:41 -0600

    Conditional whether in am or pm

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let hrs = document.getElementById('hrs');
 let mins = document.getElementById('mins')
 let secs = document.getElementById('secs')
-let am = document.getElementById('am');
+let ams = document.getElementById('am');
 
 let hr = new Date().getHours();
 let min = new Date().getMinutes();
 let sec = new Date().getSeconds();
+let am = hr >= 12 ? 'PM' : 'AM';
 
 hrs.innerHTML = hr;
 mins.innerHTML = min;
 secs.innerHTML = sec;
+ams.innerHTML = am;
