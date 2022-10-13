0922002eeb3776335da3ce09f0fcb8004868792e
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-13 16:57:32 -0600

    Add space

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
 setInterval(() => {
   
   let hrs = document.getElementById('hrs');
   let mins = document.getElementById('mins')
   let secs = document.getElementById('secs')
   let ams = document.getElementById('am');
+  
   let hr = new Date().getHours();
   let min = new Date().getMinutes();
   let sec = new Date().getSeconds();
