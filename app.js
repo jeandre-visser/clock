70a2413ae23169515b31f501df10c14e1ea63184
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-13 16:46:34 -0600

    Background color changes throughout the day

--- app.js
+++ app.js
@@ -12,187 +12,157 @@ setInterval(() => {
 
   let body = document.querySelector('body');
 
-  //test
-  if (hr >= 8 && hr < 17) {
-    body.style.backgroundColor = "#0f2027"; 
-    body.style.background = "-webkit-linear-gradient(to top, #053752, #001A26)"; 
-    body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-    body.style.color = "#FFF";
-  };
-
-
-
-  // 12am - 4am
+  // 12am - 4am //
   if (hr >= 0 && hr < 4) {
-    body.style.backgroundColor = "#F3904F"; 
-    body.style.background = "-webkit-linear-gradient(to top, #3B4371, #F3904F)"; 
+    body.style.background = "-webkit-linear-gradient(to top, #121138, #01001F)"; 
     body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #3B4371, #F3904F)";
+    body.style.background =  "linear-gradient(to top, #121138, #01001F)";
     body.style.color = "#FFF";
   };
 
-  // 4am - 5am
+  // 4am - 5am //
   if (hr >= 4 && hr < 5) {
-    body.style.backgroundColor = "#0f2027"; 
-    body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
+    body.style.background = "-webkit-linear-gradient(to top, #04006B, #01001F)"; 
     body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #053752, #001A26)";
+    body.style.background =  "linear-gradient(to top, #04006B, #01001F)";
     body.style.color = "#FFF";
   };
   
-  // 5am - 6am
+  // 5am - 6am //
   if (hr >= 5 && hr < 6) {
-    body.style.backgroundColor = "#0f2027"; 
-    body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
+    body.style.background = "-webkit-linear-gradient(to top, #95450D, #11316B, #04006B)"; 
     body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #053752, #001A26)";
+    body.style.background =  "linear-gradient(to top, #95450D, #11316B, #04006B)";
     body.style.color = "#FFF";
   };
 
-  // 6am - 7am
+  // 6am - 7am //
   if (hr >= 6 && hr < 7) {
-    body.style.backgroundColor = "#0f2027"; 
-    body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
+    body.style.background = "-webkit-linear-gradient(to top, #D9770F, #D9940F, #1153AD)"; 
     body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #053752, #001A26)";
+    body.style.background =  "linear-gradient(to top, #D9770F, #D9940F, #1153AD)";
     body.style.color = "#FFF";
   };
   
-  // 7am - 8am
+  // 7am - 8am //
   if (hr >= 7 && hr < 8) {
-    body.style.backgroundColor = "#0f2027"; 
-    body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
+    body.style.background = "-webkit-linear-gradient(to top, #1153AD, #D9940F, #D9770F)"; 
     body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #053752, #001A26)";
+    body.style.background =  "linear-gradient(to top, #1153AD, #D9940F, #D9770F)";
     body.style.color = "#FFF";
   };
 
-  // 8am - 9am
+  // 8am - 9am //
   if (hr >= 8 && hr < 9) {
-    body.style.backgroundColor = "#0f2027"; 
-    body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
+    body.style.background = "-webkit-linear-gradient(to top, #1153AD, #1565E6, #D9940F, #D9770F)"; 
     body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #053752, #001A26)";
+    body.style.background =  "linear-gradient(to top, #1153AD, #1565E6, #D9940F, #D9770F)";
     body.style.color = "#FFF";
   };
 
-  // 9am - 10am
+  // 9am - 10am //
   if (hr >= 9 && hr < 10) {
-    body.style.backgroundColor = "#0f2027"; 
-    body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
+    body.style.background = "-webkit-linear-gradient(to top, #1153AD, #1565E6, #176EFF, #EE9E21)"; 
     body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #053752, #001A26)";
+    body.style.background =  "linear-gradient(to top, #1153AD, #1565E6, #176EFF, #EE9E21)";
     body.style.color = "#FFF";
   };
 
-  // 10am - 11am
+  // 10am - 11am //
   if (hr >= 10 && hr < 11) {
-    body.style.backgroundColor = "#0f2027"; 
-    body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
+    body.style.background = "-webkit-linear-gradient(to top, #1153AD, #176EFF, #59B1FF)"; 
     body.style.zIndex = "0";
-    body.style.background =  "linear-gradient(to top, #053752, #001A26)";
+    body.style.background =  "linear-gradient(to top, #1153AD, #176EFF, #59B1FF)";
     body.style.color = "#FFF";
   };
 
-    // 11am - 12pm
-    if (hr >= 11 && hr < 12) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 11am - 12pm //
+  if (hr >= 11 && hr < 12) {
+    body.style.background = "-webkit-linear-gradient(to top, #004AFF, #176EFF, #59B1FFF)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #004AFF, #176EFF, #59B1FF)";
+    body.style.color = "#FFF";
+  };
   
-    // 12pm - 3pm
-    if (hr >= 12 && hr < 15) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 12pm - 3pm //
+  if (hr >= 12 && hr < 15) {
+    body.style.background = "-webkit-linear-gradient(to top, #004AFF, #176EFF, #314EFF)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #004AFF, #176EFF, #314EFF)";
+    body.style.color = "#FFF";
+  };
 
-    // 3pm - 4pm
-    if (hr >= 15 && hr < 16) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 3pm - 4pm //
+  if (hr >= 15 && hr < 16) {
+    body.style.background = "-webkit-linear-gradient(to top, #004AFF, #176EFF, #314EFF)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #004AFF, #314EFF, #176EFF)";
+    body.style.color = "#FFF";
+  };
 
-    // 4pm - 5pm
-    if (hr >= 16 && hr < 17) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 4pm - 5pm //
+  if (hr >= 16 && hr < 17) {
+    body.style.background = "-webkit-linear-gradient(to top, #004AFF, #31A6FF)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #004AFF, #31A6FF)";
+    body.style.color = "#FFF";
+  };
 
-    // 5pm - 6pm
-    if (hr >= 17 && hr < 18) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 5pm - 6pm //
+  if (hr >= 17 && hr < 18) {
+    body.style.background = "-webkit-linear-gradient(to top, #3075FF, #31A6FF, #31A6FF, #FF830A)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #3075FF, #31A6FF, #31A6FF, #FF830A)";
+    body.style.color = "#FFF";
+  };
 
-    // 6pm - 7pm
-    if (hr >= 18 && hr < 19) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 6pm - 7pm //
+  if (hr >= 18 && hr < 19) {
+    body.style.background = "-webkit-linear-gradient(to top, #3061FF, #3075FF, #3069FF, #31A6FF, #FF830A, #FFB419)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #3061FF, #3075FF, #3069FF, #31A6FF, #FF830A, #FFB419)";
+    body.style.color = "#FFF";
+  };
 
-    // 7pm - 8pm
-    if (hr >= 19 && hr < 20) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 7pm - 8pm //
+  if (hr >= 19 && hr < 20) {
+    body.style.background = "-webkit-linear-gradient(to top, #3061FF, #3075FF, #3069FF, #FF830A, #FFA31B, #FFB419)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #3061FF, #3075FF, #3069FF, #FF830A, #FFA31B, #FFB419)";
+    body.style.color = "#FFF";
+  };
 
-    // 8pm - 9pm
-    if (hr >= 20 && hr < 21) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 8pm - 9pm //
+  if (hr >= 20 && hr < 21) {
+    body.style.background = "-webkit-linear-gradient(to top, #3061FF,#FF5A17, #FF830A, #FFA31B, #FFB419)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #3061FF,#FF5A17, #FF830A, #FFA31B, #FFB419)";
+    body.style.color = "#FFF";
+  };
 
-    // 9pm - 10pm
-    if (hr >= 21 && hr < 22) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 9pm - 10pm //
+  if (hr >= 21 && hr < 22) {
+    body.style.background = "-webkit-linear-gradient(to top, #FFB419, #FF5A17, #2000FF, #13138F, #13138F)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #FFB419, #FF5A17, #2000FF, #13138F, #13138F)";
+    body.style.color = "#FFF";
+  };
 
-    // 10pm - 11pm //
-    if (hr >= 22 && hr < 23) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #053752, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 10pm - 11pm //
+  if (hr >= 22 && hr < 23) {
+    body.style.background = "-webkit-linear-gradient(#FF5A17, #132390, #010963, #01093E)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #FF5A17, #132390, #010963, #01093E)";
+    body.style.color = "#FFF";
+  };
 
-    // 11pm - 12pm
-    if (hr >= 23 && hr < 24) {
-      body.style.backgroundColor = "#0f2027"; 
-      body.style.background = "-webkit-linear-gradient(to top, #0f2027, #203a43, #001A26)"; 
-      body.style.zIndex = "0";
-      body.style.background =  "linear-gradient(to top, #053752, #001A26)";
-      body.style.color = "#FFF";
-    };
+  // 11pm - 12pm
+  if (hr >= 23 && hr < 24) {
+    body.style.background = "-webkit-linear-gradient(#FF5A17, #132390, #010963, #01093E)"; 
+    body.style.zIndex = "0";
+    body.style.background =  "linear-gradient(to top, #132390, #010963, #01093E)";
+    body.style.color = "#FFF";
+  };
 
   // 24 hr to 12 hr clock conversion
   if (hr > 12) {
